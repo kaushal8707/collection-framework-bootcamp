fcecec302fc154eb19f9dd9084d4ada89db03a5b
kaushal kumar <kaushal.kumar@mastercard.com>
2025-05-29 21:59:31 +0530

    first commit

---
+++ src/main/java/com/zero/to/hero/collection/frameworks/map/concurrenthashmap/ConcurrentHashMapDemo.java
@@ -0,0 +1,8 @@
+package com.zero.to.hero.collection.frameworks.map.concurrenthashmap;
+
+public class ConcurrentHashMapDemo {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/zero/to/hero/collection/frameworks/map/concurrentskiplistmap/ConcurrentSkipListMapDemo.java
@@ -0,0 +1,15 @@
+package com.zero.to.hero.collection.frameworks.map.concurrentskiplistmap;
+
+import java.util.concurrent.ConcurrentSkipListMap;
+
+public class ConcurrentSkipListMapDemo {
+    public static void main(String[] args) {
+
+        ConcurrentSkipListMap<String, Integer> concurrentSkipListMap =
+                new ConcurrentSkipListMap<>();
+
+
+
+
+    }
+}
